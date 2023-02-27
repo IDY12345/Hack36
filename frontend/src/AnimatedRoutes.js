3445b16f886d6eac3470ce9a75483c072cf17e7b
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-02-27 14:06:19 +0530

    Initial Commit

---
+++ frontend/src/AnimatedRoutes.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import React from 'react'
+
+function AnimatedRoutes() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default AnimatedRoutes
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React,{useState,useEffect} from "react"
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-ishaan">
+      Hello world!
+    </h1>
+  )
+}
