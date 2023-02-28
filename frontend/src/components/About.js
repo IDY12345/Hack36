91ac7d179ec2cf5cfa2b687af3f5a19ecfd4141c
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-02-28 22:35:36 +0530

    Added firebase

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.css'
+
 function About() {
     return (
         <div>
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -18,7 +18,7 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" to="/">
         The Pioneers
       </Link>{' '}
       {new Date().getFullYear()}
