6039c487484a3a37a86d43cf286f40a644b45970
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-03-01 18:20:55 +0530

    Added Contact Component

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,35 +1,36 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './About.css'
 import About1 from './About1';
+import Contact from './Contact';
 
 function About() {
     const [colourChange, setColourChange] = useState(false)
 
-    const backgroundChange=()=>
-    {
-        if(window.scrollY>=75)
-        {
+    const backgroundChange = () => {
+        if (window.scrollY >= 75) {
             setColourChange(true);
         }
-        else
-        {
+        else {
             setColourChange(false)
         }
     }
-    
+
     useEffect(() => {
-      backgroundChange()
-    
-        window.addEventListener("scroll",backgroundChange)
+        backgroundChange()
+
+        window.addEventListener("scroll", backgroundChange)
     }, [])
-    
+
     return (
-        <div className={colourChange ? 'Back-div':'Back-div active'}>
-            <div className={colourChange?'Image-div':'Image-div active'}>
-                <img src='Assets\logo1.png' className='About-Image' />
-                <p className='presents'>Presents</p>
+        <div>
+            <div className={colourChange ? 'Back-div' : 'Back-div active'}>
+                <div className={colourChange ? 'Image-div' : 'Image-div active'}>
+                    <img src='Assets\logo1.png' className='About-Image' />
+                    <p className='presents'>Presents</p>
+                </div>
+                <About1 />                
             </div>
-            <About1 />
+            <Contact />
         </div>
     )
 }
