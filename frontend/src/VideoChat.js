be0c1f67b249de5405be25b6c0627aa25b87e6c2
Ninad3721 <ninadtrivedi25@gmail.com>
2023-05-09 23:15:08 +0530

    Integrated video chat feature with website

--- frontend/src/VideoChat.js
+++ frontend/src/VideoChat.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { HuddleIframe } from "@huddle01/huddle01-iframe";
+import { Button } from '@mui/material';
+import "./VideoChat.css"
+import { Link } from 'react-router-dom';
 const iframeConfig = {
     roomUrl: "https://iframe.huddle01.com",
-    height: "600px",
-    width: "80%",
+    height: "700px",
+    width: "100%",
     noBorder: true,
     position: "relative",
     left: "-100px",
@@ -14,6 +17,11 @@ function VideoChat() {
     return (
         <div>
             <HuddleIframe config={iframeConfig} style={{ position: "relative", left: "-100px", top: "-100px" }} />
+            <div className='button-container'>
+                <Link to="/home"><Button variant="contained" color="error"> Return to home </Button></Link>
+
+            </div>
+
         </div>
     )
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,18 +2,17 @@ import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import './Navbar.css'
 import AnimatedRoutes from '../AnimatedRoutes'
+import { ConnectButton } from "web3uikit"
 
+function Navbar({ setIsAuth, isAuth }) {
 
-function Navbar({setIsAuth,isAuth}) {
-
-  const signUserOut=()=>
-  {
+  const signUserOut = () => {
     localStorage.clear();
     setIsAuth(false);
-    window.location.pathname="/SignIn";
+    window.location.pathname = "/SignIn";
   }
 
-console.log(isAuth)
+  console.log(isAuth)
   return (
     <div>
       <Router>
@@ -26,23 +25,23 @@ console.log(isAuth)
                 <button className='Logo-btn'>Pioneers</button>
               </div>
               <div className='Navigation-Tools'>
-                 {
-                 !isAuth?(<></>):( 
-              <div className='Services' title='Services'>
-                  <button className='Services-btn'>
-                    Services <i class="fa fa-caret-down" aria-hidden="true" id='drop'></i>
-                  </button>
+                {
+                  !isAuth ? (<></>) : (
+                    <div className='Services' title='Services'>
+                      <button className='Services-btn'>
+                        Services <i class="fa fa-caret-down" aria-hidden="true" id='drop'></i>
+                      </button>
 
-                  <div className="Services-Content">
-                    <Link to="/Register"><button className="Service1">Register Company</button></Link>
-                    <Link to="/Offset"><button className="Service2">Green Organisation Registration</button></Link>
-                  </div>
-                 </div>
-                )} 
+                      <div className="Services-Content">
+                        <Link to="/Register"><button className="Service1">Register Company</button></Link>
+                        <Link to="/Offset"><button className="Service2">Green Organisation Registration</button></Link>
+                      </div>
+                    </div>
+                  )}
                 <div className="Home" title='Home'>
-                  {!isAuth ?(<></>) :( 
-                  <Link to="/Home" className="Home-btn-link"><button class="Home-btn">Home</button></Link>
-                   )} 
+                  {!isAuth ? (<></>) : (
+                    <Link to="/Home" className="Home-btn-link"><button class="Home-btn">Home</button></Link>
+                  )}
                 </div>
                 <div className="About" title='About'>
                   <Link to="/">
@@ -50,26 +49,27 @@ console.log(isAuth)
                   </Link>
                 </div>
                 <div className="Login" title='Login'>
-                  {isAuth?(<></>):(
-                  <Link to="/SignIn"><button className="SignIn-btn">Login</button></Link>
+                  {isAuth ? (<></>) : (
+                    <Link to="/SignIn"><button className="SignIn-btn">Login</button></Link>
                   )}
                 </div>
                 <div >
-                {isAuth?(<></>):(
-                  <Link to="/SignUp"><button className='SignIn-btn'>Sign Up</button></Link>
+                  {isAuth ? (<></>) : (
+                    <Link to="/SignUp"><button className='SignIn-btn'>Sign Up</button></Link>
                   )}
                 </div>
-                {!isAuth?(<></>):(
-                <div className='Profile' >
-                  <button className='Profile-btn' title='Profile'><i class="fa-solid fa-user"></i></button>
+                <ConnectButton />
+                {!isAuth ? (<></>) : (
+                  <div className='Profile' >
+                    <button className='Profile-btn' title='Profile'><i class="fa-solid fa-user"></i></button>
 
-                  <div className='Profile-Content'>
-                    <Link to="/Profile"><button className='Profile1'>Profile</button></Link>
-                    <Link to="/Edit-Profile"><button className='Profile2'>Edit Profile</button></Link>
-                    <Link to="/CarbonUpdate"><button className='Profile3'>Carbon Credits Update</button></Link>
-                    <Link to="/SignIn"><button className='Profile4'  onClick={signUserOut}>Logout</button></Link>
+                    <div className='Profile-Content'>
+                      <Link to="/Profile"><button className='Profile1'>Profile</button></Link>
+                      <Link to="/Edit-Profile"><button className='Profile2'>Edit Profile</button></Link>
+                      <Link to="/CarbonUpdate"><button className='Profile3'>Carbon Credits Update</button></Link>
+                      <Link to="/SignIn"><button className='Profile4' onClick={signUserOut}>Logout</button></Link>
+                    </div>
                   </div>
-                </div>
                 )}
               </div>
 
@@ -79,7 +79,7 @@ console.log(isAuth)
             </div>
           </div>
         </nav>
-        <AnimatedRoutes  setIsAuth={setIsAuth} isAuth={isAuth} />
+        <AnimatedRoutes setIsAuth={setIsAuth} isAuth={isAuth} />
       </Router>
     </div>
   )
