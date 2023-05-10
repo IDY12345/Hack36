e1fc83dca435202eefbebc402fe6ba35809422ec
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-05-10 16:53:55 +0530

    Changes in Animated Routes

--- frontend/src/AnimatedRoutes.js
+++ frontend/src/AnimatedRoutes.js
@@ -11,9 +11,9 @@ import CarbonExchange from "./components/CarbonExchange";
 import CarbonUpdate from "./components/CarbonUpdate";
 import OffsetDescription from "./components/OffsetDescription";
 import Login from "./components/SignIn";
-<<<<<<< HEAD
 import CarbonCredits from "./components/CarbonCredits";
 import Climate from "./components/Climate";
+import VideoChat from "./VideoChat";
 function AnimatedRoutes({setIsAuth,isAuth}) {
     // const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"))
     const location=useLocation();
@@ -31,27 +31,9 @@ function AnimatedRoutes({setIsAuth,isAuth}) {
             <Route path="/OffsetBuy" element={<OffsetDescription isAuth={isAuth} />} />
             <Route path="/CarbonCredits" element={<CarbonCredits />} />
             <Route path="/Climate" element={<Climate />} />
+            <Route path="/VideoChat" element={<VideoChat />}></Route>
         </Routes>
-=======
-import VideoChat from "./VideoChat";
-function AnimatedRoutes({ setIsAuth, isAuth }) {
-  // const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"))
-  const location = useLocation();
-  return (
-    <AnimatePresence>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/Home" element={<Home isAuth={isAuth} />} />
-        <Route path="/SignUp" element={<SignUp isAuth={isAuth} />} />
-        <Route path="/SignIn" element={<Login setIsAuth={setIsAuth} isAuth={isAuth} />} />
-        <Route path="/" element={<About isAuth={isAuth} />} />
-        <Route path="/Register" element={<RegisterCompany isAuth={isAuth} />} />
-        <Route path="/Offset" element={<Offset isAuth={isAuth} />} />
-        <Route path="/CarbonExchange" element={<CarbonExchange isAuth={isAuth} />} />
-        <Route path="/CarbonUpdate" element={<CarbonUpdate isAuth={isAuth} />} />
-        <Route path="/OffsetBuy" element={<OffsetDescription isAuth={isAuth} />} />
-        <Route path="/VideoChat" element={<VideoChat />}></Route>
-      </Routes>
->>>>>>> be0c1f67b249de5405be25b6c0627aa25b87e6c2
+
     </AnimatePresence>
   )
 }
