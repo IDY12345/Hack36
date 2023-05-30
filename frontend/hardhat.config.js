c078392d1678790cf1d1f828a1223fa3363bac6f
Boggiefottie <bhavyapratapsingh240203@gmail.com>
2023-05-31 00:19:15 +0530

    "Decentralized drive"

--- frontend/hardhat.config.js
+++ frontend/hardhat.config.js
@@ -1,18 +1,31 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 /** @type import('hardhat/config').HardhatUserConfig */
-require('dotenv').config();
+require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 
 const { API_URL, PRIVATE_KEY } = process.env;
-console.log(API_URL)
+console.log(API_URL);
 module.exports = {
-    solidity: "0.8.18",
-    defaultNetwork: "goerli",
-    networks: {
-        hardhat: {},
-        goerli: {
-            url: "https://eth-goerli.g.alchemy.com/v2/oqeOc94obCxkJEpe3KNrK3MzHE6Y3pmy",
-            accounts: [`0x11a1cea1dd86cc5e6f48ba97a0e9651f7ec2fd9f38943c13ccfb4d6c88c4cb36`]
-        }
+  solidity: "0.8.18",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/oqeOc94obCxkJEpe3KNrK3MzHE6Y3pmy",
+      accounts: [
+        `0x11a1cea1dd86cc5e6f48ba97a0e9651f7ec2fd9f38943c13ccfb4d6c88c4cb36`,
+      ],
     },
+    xinfin: {
+      url: process.env.XINFIN_NETWORK_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    apothem: {
+      url: process.env.APOTHEM_NETWORK_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  paths: {
+    artifacts: "./src/artifacts",
+  },
 };
