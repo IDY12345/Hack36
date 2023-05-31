68c620e4e8a871909ccb1b1ccacf61e256d6652f
Boggiefottie <104435306+Boggiefottie@users.noreply.github.com>
2023-05-31 22:43:02 +0530

    Updated hardhat.config.js for XDC

--- frontend/hardhat.config.js
+++ frontend/hardhat.config.js
@@ -13,6 +13,18 @@ module.exports = {
         goerli: {
             url: "https://eth-goerli.g.alchemy.com/v2/BvQ8bEoXvE-AZnOoswN0nFkhfYEg8Lr3",
             accounts: [`5a972752a68fdab77079d2f9229e24eac70de35fd3502b5fd4c6faef5073bcbc`]
-        }
+        },
+    xinfin: {
+    url: process.env.XINFIN_NETWORK_URL,
+    accounts: [process.env.PRIVATE_KEY],
     },
+    apothem: {
+    url: process.env.APOTHEM_NETWORK_URL,
+    accounts: [process.env.PRIVATE_KEY],
+    },
+    
+  }, 
+  paths: {
+        artifacts: "./src/artifacts",
+  },  
 };
